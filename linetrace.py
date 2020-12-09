8621a48defe292eecb50864ad7d1504ecc629e70
AD58-3104 <s19C1012SK@s.chibakoudai.jp>
2020-12-09 17:33:05 +0900

    remove 2byte character

--- linetrace.py
+++ linetrace.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BCM)
 
 sensor_right = 7
 sensor_left = 6
-motor_right = 13#指定
+motor_right = 13
 motor_left = 14
 GPIO.setup(motor_right, GPIO.OUT)
 GPIO.setup(motor_left, GPIO.OUT)
