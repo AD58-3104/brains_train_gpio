f29b4a2c6d5d37f262e36f1041a29d67ca74efbc
AD58-3104 <s19C1012SK@s.chibakoudai.jp>
2020-12-09 17:07:24 +0900

    add clockwise and counterclockwise

--- motor_pwm.py
+++ motor_pwm.py
@@ -4,16 +4,24 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-motor_pin = 23#指定
+motor_pin = 13 #12
+motor_in1 = 17
+motor_in2 = 27
 GPIO.setup(motor_pin, GPIO.OUT)
+GPIO.setup(motor_in1, GPIO.OUT)
+GPIO.setup(motor_in2, GPIO.OUT)
 
 servo = GPIO.PWM(motor_pin, 50)
 servo.start(0)
 
 for i in range(3):
-    servo.ChangeDutyCycle(5)
+    GPIO.output(motor_in1, GPIO.HIGH)
+    GPIO.output(motor_in2, GPIO.LOW)
+    servo.ChangeDutyCycle(30)
     time.sleep(1)
 
+    GPIO.output(motor_in1, GPIO.LOW)
+    GPIO.output(motor_in2, GPIO.HIGH)
     servo.ChangeDutyCycle(30)
     time.sleep(1)
 
