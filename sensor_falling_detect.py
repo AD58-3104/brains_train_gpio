f57596bdc31598abd5dcca73d686dee0f303eb29
AD58-3104 <s19C1012SK@s.chibakoudai.jp>
2020-12-10 16:47:00 +0900

    add some file

---
+++ sensor_falling_detect.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)                         
+
+sensor_right = 22
+sensor_left = 9
+GPIO.setup(sensor_left,GPIO.IN)                                                       
+GPIO.setup(sensor_right,GPIO.IN)                                                       
+
+
+def switch_callback(gpio_pin):
+    print(gpio_pin)
+    print("falling")
+
+
+
+GPIO.add_event_detect(sensor_left, GPIO.FALLING,bouncetime=100)
+GPIO.add_event_detect(sensor_right, GPIO.FALLING,bouncetime=100)
+
+GPIO.add_event_callback(sensor_left, switch_callback)     
+GPIO.add_event_callback(sensor_right, switch_callback)   
+
+try:
+    while True:
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
