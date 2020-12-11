36d4ddcd7e55157b380153f99a648c09169be73d
AD58-3104 <s19C1012SK@s.chibakoudai.jp>
2020-12-12 00:03:03 +0900

    add comment

--- linetrace.py
+++ linetrace.py
@@ -3,8 +3,10 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+#日本語を入れるとエラーが出るので使う時日本語コメントは消す
 
 #--------------pin declaration--------------------
+#ピン番号の宣言
 motor_pin_right = 13
 motor_right_in1 = 17 
 motor_right_in2 = 27
@@ -17,6 +19,7 @@ sensor_left = 9
 #--------------------------------------------------
 
 #----------------pin setup------------------------------
+#各ピンの設定をしている。
 GPIO.setup(motor_pin_right, GPIO.OUT)
 GPIO.setup(motor_right_in1, GPIO.OUT)
 GPIO.setup(motor_right_in2, GPIO.OUT)
@@ -29,39 +32,43 @@ GPIO.setup(motor_left_in2, GPIO.OUT)
 GPIO.setup(sensor_left,GPIO.IN)
 #---------------------------------------------------------
 
+#---------------pwm object-------------------
+#pwmオブジェクトの宣言
+servo_left = GPIO.PWM(motor_pin_left, 100)
+servo_right = GPIO.PWM(motor_pin_right, 100)
+#--------------------------------------------
+
 
 #---------------------event detect-------------------------------------
+#あるピンでイベントが発生した時に何か処理をする事を宣言
 GPIO.add_event_detect(sensor_left, GPIO.FALLING,bouncetime=50)
 GPIO.add_event_detect(sensor_right, GPIO.FALLING,bouncetime=50)
 
-
+#呼ばれる関数の定義
 def switch_callback_right(gpio_pin):
     print("right_falling")
-    GPIO.output(gpio_pin, GPIO.HIGH)
-    GPIO.output(gpio_pin, GPIO.LOW)
-    servo_right.ChangeDutyCycle(30)
+    servo_right.ChangeDutyCycle(50)
+    servo_left.ChangeDutyCycle(30)
 
 def switch_callback_left(gpio_pin):
     print("left_falling")
-    GPIO.output(gpio_pin, GPIO.LOW)
-    GPIO.output(gpio_pin, GPIO.HIGH)
     servo_right.ChangeDutyCycle(30)
+    servo_left.ChangeDutyCycle(50)
 
-
+#イベントが起こった時に呼ばれる関数を登録
 GPIO.add_event_callback(sensor_left, switch_callback_left)     
 GPIO.add_event_callback(sensor_right, switch_callback_right)   
 #------------------------------------------------------------------------
 
 
-#---------------pwm object-----------------------------
-servo_left = GPIO.PWM(motor_pin_left, 100)
-servo_right = GPIO.PWM(motor_pin_right, 100)
+#----------pwm start----------------------------
+#モータの動作開始
 servo_right.start(30)
 servo_left.start(30)
 #--------------------------------------------
 
-
 #-----------waiting event (main function)---------------------------
+#メイン関数の様になる部分。ここがループして上で定義したイベントが起こるのを待ち、イベントが起こったら関数を呼ぶ。
 try:
     while True:
         time.sleep(0.05)
